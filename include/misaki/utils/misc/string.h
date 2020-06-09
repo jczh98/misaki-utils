61f4ec0b138d5df6581991967e057d882a05eeb2
neverfelly <neverfelly@gmail.com>
2020-06-09 19:27:30 +0800

    Update string.h
    Signed-off-by: neverfelly <neverfelly@gmail.com>

--- include/misaki/utils/misc/string.h
+++ include/misaki/utils/misc/string.h
@@ -1,7 +1,24 @@
+#include <algorithm>
 #include <string>
 #include <vector>
 
 namespace misaki::string {
+
+// Return a lower-case version of the given string (warning: not unicode compliant)
+inline std::string to_lower(const std::string &s) {
+  std::string result;
+  result.resize(s.length());
+  std::transform(s.begin(), s.end(), result.begin(), ::tolower);
+  return result;
+}
+
+// Return a upper-case version of the given string (warning: not unicode compliant)
+inline std::string to_upper(const std::string &s) {
+  std::string result;
+  result.resize(s.length());
+  std::transform(s.begin(), s.end(), result.begin(), ::toupper);
+  return result;
+}
 
 inline bool starts_with(const std::string &string, const std::string &prefix) {
   if (prefix.size() > string.size())
@@ -23,11 +40,11 @@ extern std::string indent(const std::string &string, size_t amount = 2);
 
 inline void replace_inplace(std::string &str, const std::string &source,
                             const std::string &target) {
-    size_t pos = 0;
-    while ((pos = str.find(source, pos)) != std::string::npos) {
-        str.replace(pos, source.length(), target);
-        pos += target.length();
-    }
+  size_t pos = 0;
+  while ((pos = str.find(source, pos)) != std::string::npos) {
+    str.replace(pos, source.length(), target);
+    pos += target.length();
+  }
 }
 
 }  // namespace misaki::string
